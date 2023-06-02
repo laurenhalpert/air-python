399d33607011d6a66974c0f84f45f62cb6f13e73
Lauren <halpertln@gmail.com>
2023-06-02 10:34:58 -0600

    delete commented out code

--- lib/models.py
+++ lib/models.py
@@ -8,12 +8,6 @@ engine = create_engine('sqlite:///trip.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# convention = {
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# }
-# metadata = MetaData(naming_convention=convention)
-
-# Base = declarative_base(metadata=metadata)
 
 Base = declarative_base()
 
